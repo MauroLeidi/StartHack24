bf6a060a0142ad5b44735699978e771323a47521
JohannesGetzner <j.getzner@gmail.com>
2024-03-21 12:28:30 +0100

    improvedments to charting api

--- app/charting.py
+++ app/charting.py
@@ -36,41 +36,69 @@ def add_ee_layer(self, ee_object, vis_params, name):
 folium.Map.add_ee_layer = add_ee_layer
 
 
-def get_brazil_landcover():
-    brazil_shapefile = geemap.shp_to_ee('../data/Brazil/Brazil.shp')
-    landcover = ee.Image('MODIS/006/MCD12Q1/2004_01_01').select('LC_Type1').clip(brazil_shapefile)
-    igbpLandCoverVis = {
-        'min': 1.0,
-        'max': 17.0,
-        'palette': [
-            '05450a', '086a10', '54a708', '78d203', '009900', 'c6b044',
-            'dcd159', 'dade48', 'fbff13', 'b6ff05', '27ff87', 'c24f44',
-            'a5a5a5', 'ff6d4c', '69fff8', 'f9ffa4', '1c0dff',
-        ],
-    }
-    return landcover, igbpLandCoverVis
-
-
-def get_landcover_by_years(years: List[str]):
+def get_chart_by_layers(layers: List[str]):
     init_ee()
     brazil_shapefile = geemap.shp_to_ee('../data/Brazil/Brazil.shp')
     brazil_map = folium.Map(location=[-14.5, -51], zoom_start=4, height=500)
-    igbpLandCoverVis = {
-        'min': 1.0,
-        'max': 17.0,
-        'palette': [
-            '05450a', '086a10', '54a708', '78d203', '009900', 'c6b044',
-            'dcd159', 'dade48', 'fbff13', 'b6ff05', '27ff87', 'c24f44',
-            'a5a5a5', 'ff6d4c', '69fff8', 'f9ffa4', '1c0dff',
-        ],
-    }
-    for year in years:
-        lc = ee.Image(f'MODIS/006/MCD12Q1/{year}_01_01').select('LC_Type1')
-        lc = lc.clip(brazil_shapefile)
-        brazil_map.add_ee_layer(lc, igbpLandCoverVis, str(year))
-    #brazil_map.addLayerControl()
+    for layer in layers:
+        if layer.startswith("landcover"):
+            year = layer.split("_")[1]
+            lc = ee.Image(f'MODIS/006/MCD12Q1/{year}_01_01').select('LC_Type1')
+            lc = lc.clip(brazil_shapefile)
+            igb = {
+                'min': 1.0,
+                'max': 17.0,
+                'palette': [
+                    '05450a', '086a10', '54a708', '78d203', '009900', 'c6b044',
+                    'dcd159', 'dade48', 'fbff13', 'b6ff05', '27ff87', 'c24f44',
+                    'a5a5a5', 'ff6d4c', '69fff8', 'f9ffa4', '1c0dff',
+                ],
+            }
+            layer_name = f"Land Cover {year}"
+        elif layer.startswith("population"):
+            # year = layer.split("_")[1]
+            dataset = ee.ImageCollection('WorldPop/GP/100m/pop')
+            igb = {
+                'bands': ['population'],
+                'min': 0.0,
+                'max': 50.0,
+                'palette': ['24126c', '1fff4f', 'd4ff50']
+            }
+            lc = dataset.mean()
+            lc = lc.clip(brazil_shapefile)
+            layer_name = 'Population'
+        elif layer.startswith(f'burn'):
+            # year = layer.split("_")[1]
+            dataset = ee.ImageCollection('MODIS/061/MCD64A1').filter(ee.Filter.date('2017-01-01', '2018-05-01'))
+            burnedArea = dataset.select('BurnDate')
+            fc = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017').filter(
+                'country_na == "Brazil"'
+            )
+            lc = burnedArea.map(lambda img: img.clipToCollection(fc))
+            igb = {
+                'min': 30.0,
+                'max': 341.0,
+                'palette': ['4e0400', '951003', 'c61503', 'ff1901']
+            }
+            layer_name = "Burned Area"
+        elif layer.startswith("Brazil"):
+            style = {
+                "color": "00ff00",  # Outline color
+                "fillColor": "0000ff60",  # Interior color with opacity (60 at the end represents opacity in hex)
+                "width": 2,  # Outline width
+            }
+            brazil_shapefile = brazil_shapefile.style(**style)  # Apply the style
+            layer_name = "Brazil"
+            brazil_map.add_ee_layer(brazil_shapefile, {}, layer_name)
+        elif layer.startswith("biomes"):
+            lc = geemap.shp_to_ee('../data/Brazil Biomes/Brazil_biomes.shp')
+            style = {
+                "color": "ff0000",  # Outline color
+                "fillColor": "ffff0060",  # Interior color with opacity
+                "width": 1,  # Outline width
+            }
+            lc = lc.style(**style)  # Apply the style
+            layer_name = "Biomes"
+            brazil_map.add_ee_layer(lc, {}, layer_name)
     brazil_map.add_child(folium.LayerControl())
-    #landcover = ee.Image('MODIS/006/MCD12Q1/2004_01_01').select('LC_Type1')
-    #brazil_lc = landcover.clip(brazil_shapefile)
-    #brazil_map.add_ee_layer(brazil_lc, igbpLandCoverVis, 'Land Cover')
     return brazil_map._repr_html_()
